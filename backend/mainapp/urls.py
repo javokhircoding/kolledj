d3f33cc0465680cf04b7f0c9d2ec79fce88eabef
Javoxir Shermatov <javokhir.coding@gmail.com>
2025-11-26 16:43:25 +0500

    initial setup done, till the frontend part, I can continue once the frontend part is done

--- backend/mainapp/urls.py
+++ backend/mainapp/urls.py
@@ -8,4 +8,8 @@ urlpatterns = [
     path('yangiliklar/', YangiliklarView.as_view(), name='yangiliklar'),
     path('elonlar/<slug:slug>/', ElonView.as_view(), name='elon'),
     path('yangiliklar/<slug:slug>/', YangilikView.as_view(), name='yangilik'),
+    path('tarix/', TarixView.as_view(), name='tarix'),
+    path('yo`nalishlar/', YunalishView.as_view(), name='yunalishlar'),
+    path('rahbariyat/', RaxbariyatView.as_view(), name='rahbariyat'),
+    path('hamkorlik/', HamkorlikView.as_view(), name='hamkorlik'),
 ]
