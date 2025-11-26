712590c3342375f79de8db9838862ce1d9d9277c
Javoxir Shermatov <javokhir.coding@gmail.com>
2025-11-27 01:41:21 +0500

    major update

--- backend/kolledj/settings.py
+++ backend/kolledj/settings.py
@@ -136,15 +136,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-#MEDIA_URL = 'media/'
-#MEDIA_ROOT = BASE_DIR / 'media'
-
 
 STATIC_URL = 'static/'
 STATIC_ROOT = BASE_DIR / "staticfiles"
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'templates/'),
+    os.path.join(BASE_DIR, 'static/'),
 ]
         
 
@@ -152,15 +149,3 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
-
-REST_FRAMEWORK = {
-    "DEFAULT_AUTHENTICATION_CLASSES" : [
-#        'rest_framework.authentication.SessionAuthentication',
-    ],
-    "DEFAULT_PERMISSION_CLASSES" : [
-        "rest_framework.permissions.AllowAny",
-    ],
-    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
-    'PAGE_SIZE': 10
-}
