5ec6a48a2f5690abfb97bd1b1460f0724af206db
Javoxir Shermatov <javokhir.coding@gmail.com>
2025-11-30 22:37:00 +0500

    finally worked in a hardcode

---
+++ backend/static/js/slider.js
@@ -0,0 +1,35 @@
+let slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  let i;
+  const slides = document.getElementsByClassName("mySlides");
+  const dots = document.getElementsByClassName("dot");
+
+  if (slides.length === 0) return;
+
+  if (n > slides.length) slideIndex = 1;
+  if (n < 1) slideIndex = slides.length;
+
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+
+  for (i = 0; i < dots.length; i++) {
+    dots[i].style.backgroundColor = "#bbb";
+  }
+
+  slides[slideIndex - 1].style.display = "block";
+
+  if (dots.length > 0) {
+    dots[slideIndex - 1].style.backgroundColor = "#717171";
+  }
+}
